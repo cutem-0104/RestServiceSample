4cfe5f1e7f157ba1bbca791a165d921e23677477
cutem-0104 <shuhei.sugaya@gmail.com>
2020-04-28 01:03:51 +0900

    uroboroSQL導入

--- src/main/java/com/example/restservice/DollController.java
+++ src/main/java/com/example/restservice/DollController.java
@@ -2,19 +2,45 @@ package com.example.restservice;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import javax.sql.DataSource;
 
-@RestController
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jp.co.future.uroborosql.SqlAgent;
+import jp.co.future.uroborosql.UroboroSQL;
+import jp.co.future.uroborosql.config.SqlConfig;
+
+@Controller
 public class DollController {
     @Autowired
-    private DollRepository dollRepository;
+    DollRepository dollRepository;
+
+    @Autowired
+    protected DataSource dataSource;
 
     @ResponseBody
-    @GetMapping("/dolls")
+    @RequestMapping(value = "/uroborosql/dolls", method = RequestMethod.GET)
+    public List<Doll> get() {
+        // DataSource取得
+        SqlConfig config = UroboroSQL.builder(dataSource).build();
+        // SQLの場所はsrc/main/resources/sql/hoge/selectAll.sql
+        // 「sql/hoge/selectAll.sql」で指定するとNot Foundになった。IntelliJからCtrl + クリックでファイル参照できないのが不便
+        List<Doll> hoge;
+        try (SqlAgent agent = config.agent()) {
+            hoge = agent.query("doll/selectAll")
+                    .collect(Doll.class);
+        }
+        return hoge;
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/dolls", method = RequestMethod.GET)
     public List<Doll> dolls() {
-        return dollRepository.findAll();
+        List<Doll> dollList = dollRepository.findAll();
+        return dollList;
     }
 }
